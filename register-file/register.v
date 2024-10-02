20afb49dfdb60e257bc92ea52ca2f5507baf4038
Francisco Casiano <personal@franciscocasiano.com>
2024-10-01 20:38:33 -0400

    Renamed wires to use uppercase

--- register-file/register.v
+++ register-file/register.v
@@ -1,16 +1,16 @@
 // 32bit
 module register(
-     input wire clk,
-     input wire reset,
-     input wire load,
+     input wire CLK,
+     input wire RESET,
+     input wire LOAD,
      input wire [31:0] d,
      output reg [31:0] q;
 );
 
-    always @(posedge clk or posedge reset) begin
-        if (reset)
+    always @(posedge CLK or posedge RESET) begin
+        if (RESET)
             q <= 32'b0;
-        else if (load)
+        else if (LOAD)
             q <= d;
     end
 
