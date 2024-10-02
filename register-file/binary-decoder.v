06c30ad2b95073bf45b981db149b777e87dfac1a
Francisco Casiano <personal@franciscocasiano.com>
2024-10-01 20:51:24 -0400

    Added enable to the binary decoder

--- register-file/binary-decoder.v
+++ register-file/binary-decoder.v
@@ -1,30 +1,33 @@
 module binary_decoder(
     input wire [3:0] in,
+    input wire ENABLE,
     output reg [15:0] out
 );
 
     always @(*) begin
-        out = 16'b0000_0000_0000_0000;
-        
-        case (in)
-            4'b0000: out[0] = 1'b1;
-            4'b0001: out[1] = 1'b1;
-            4'b0010: out[2] = 1'b1;
-            4'b0011: out[3] = 1'b1;
-            4'b0100: out[4] = 1'b1;
-            4'b0101: out[5] = 1'b1;
-            4'b0110: out[6] = 1'b1;
-            4'b0111: out[7] = 1'b1;
-            4'b1000: out[8] = 1'b1;
-            4'b1001: out[9] = 1'b1;
-            4'b1010: out[10] = 1'b1;
-            4'b1011: out[11] = 1'b1;
-            4'b1100: out[12] = 1'b1;
-            4'b1101: out[13] = 1'b1;
-            4'b1110: out[14] = 1'b1;
-            4'b1111: out[15] = 1'b1;
-            default: out = 16'b0000_0000_0000_0000; // All outputs that for some reason fail to be decoded should be zero?
-        endcase 
+        if (ENABLE) begin
+            case (in)
+                4'b0000: out = 16'b0000_0000_0000_0001;
+                4'b0001: out = 16'b0000_0000_0000_0010;
+                4'b0010: out = 16'b0000_0000_0000_0100;
+                4'b0011: out = 16'b0000_0000_0000_1000;
+                4'b0100: out = 16'b0000_0000_0001_0000;
+                4'b0101: out = 16'b0000_0000_0010_0000;
+                4'b0110: out = 16'b0000_0000_0100_0000;
+                4'b0111: out = 16'b0000_0000_1000_0000;
+                4'b1000: out = 16'b0000_0001_0000_0000;
+                4'b1001: out = 16'b0000_0010_0000_0000;
+                4'b1010: out = 16'b0000_0100_0000_0000;
+                4'b1011: out = 16'b0000_1000_0000_0000;
+                4'b1100: out = 16'b0001_0000_0000_0000;
+                4'b1101: out = 16'b0010_0000_0000_0000;
+                4'b1110: out = 16'b0100_0000_0000_0000;
+                4'b1111: out = 16'b1000_0000_0000_0000;
+                default: out = 16'b0000_0000_0000_0000;
+            endcase
+        end else begin
+            out = 16'b0000_0000_0000_0000;  // All outputs are 0 when disabled
+        end
     end
 
 endmodule
